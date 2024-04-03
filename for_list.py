1bf62e2120a55724b346471ef20fb21d12d431da
MD SIFAT ALI <sifatali520@gmail.com>
2024-04-03 06:09:33 +0600

    Update for_list.py

--- for_list.py
+++ for_list.py
@@ -1,18 +1,14 @@
 alist = [['a', 10], ['b', 2], ['c', 30], ['d', 4]]
 x = []
 y = []
-out = []
 
 for i in alist:
     x.append (i[-1])
 
 x.sort()
-# print(x)
 for i in alist:
     for j in x[:2]:
         if j == i[-1]:
             y.append(i)
-for i in y: 
-    out.append(i[0])
-print(*out)
+print(y)
 
