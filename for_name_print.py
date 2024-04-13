c33f2e427e61620a28f706489bb382b73dc8f243
MD SIFAT ALI <sifatali520@gmail.com>
2024-04-14 03:57:06 +0600

    Update for_name_print.py

--- for_name_print.py
+++ for_name_print.py
@@ -1,4 +1,10 @@
-names = ["sifat", "sajid", "moin","ripon"]
+# names = ["sifat", "sajid", "moin","ripon"]
 
-for name in names:
-    print(name)
+# for name in names:
+#     print(name)
+
+#square 
+alist =  [ 2,3,4,6,7,9,11]
+for i in alist:
+    square = i ** 2
+    print(square)
